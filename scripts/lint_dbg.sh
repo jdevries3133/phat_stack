551ed3b98d8f0e310020197a02ef991a22c9977e
Jack DeVries <jdevries3133@gmail.com>
2025-07-15 17:43:53 -0400

    fix: lint_dbg script

--- scripts/lint_dbg.sh
+++ scripts/lint_dbg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -z "$(grep -rnc 'dbg!' src | grep -v ':0$')" ]
+if [ ! -z "$(grep -rnc 'dbg!' crates | grep -v ':0$')" ]
 then
     echo "Fatal: found lingering dbg! statements!"
     exit 1
